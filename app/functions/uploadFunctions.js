49e83bff73c995c5318705dab3087f33e6935a19
sarthak <sarthak1singhal@gmail.com>
2020-08-01 19:48:43 +0530

    S

--- app/functions/uploadFunctions.js
+++ app/functions/uploadFunctions.js
@@ -56,22 +56,56 @@ module.exports = {
         console.log(fb_id)
         if (fb_id && video && gif) {
 
+            var buf_video = Buffer.from(video, 'base64');
+            var buf_gif = Buffer.from(gif, 'base64');
+            var buf_img = Buffer.from(thumb, 'base64');
 
             filename = (new Date()).getTime().toString(36) + shortid.generate();
 
             console.log(filename, "FILENAME")
-            const params = {
+            const video_params = {
                 Bucket: BUCKET_NAME,
-                Key: filename + ".mp4",
-                Body: video
+                Key: "vid_" + filename + ".mp4 ",
+                Body: buf_video
             };
 
-            s3bucket.upload(params, function(error, data) {
+
+            const gif_params = {
+                Bucket: BUCKET_NAME,
+                Key: "gif_" + filename + ".gif",
+                Body: buf_gif
+            };
+
+            const img_params = {
+                Bucket: BUCKET_NAME,
+                Key: "img_" + filename + ".jpg",
+                Body: buf_img
+            };
+
+
+
+
+            s3bucket.upload(video_params, function(error, data) {
                 if (error) throw error
                 console.log(`File uploaded successfully at ${data.Location}`)
                 console.log(data, "IS DATA")
                 console.log(req.body.category, "is categiry")
             });
+
+            s3bucket.upload(gif_params, function(error, data) {
+                if (error) throw error
+                console.log(`File uploaded successfully at ${data.Location}`)
+                console.log(data, "IS DATA")
+                console.log(req.body.category, "is categiry")
+            });
+
+            s3bucket.upload(img_params, function(error, data) {
+                if (error) throw error
+                console.log(`File uploaded successfully at ${data.Location}`)
+                console.log(data, "IS DATA")
+                console.log(req.body.category, "is categiry")
+            });
+
 
 
 
