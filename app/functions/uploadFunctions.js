e2e42b9018ab3af629760ebb2c4401a23ec18043
sarthak <sarthak1singhal@gmail.com>
2020-08-01 17:17:23 +0530

    S

--- app/functions/uploadFunctions.js
+++ app/functions/uploadFunctions.js
@@ -1,52 +1,64 @@
 // app/routes.js
 var uuid = require('uuid');
 const https = require('request')
- 
- var path = require('path');
- 
+
+var path = require('path');
+
 const AWS = require('aws-sdk');
 const fs = require('fs');
 readJson = require("r-json");
 const config = readJson(`config.json`);
 
-AWS.config.update({
-    accessKeyId: config.awsAccessID,
+
+const BUCKET_NAME = 'juneappbucket';
+
+const s3bucket = new AWS.S3({
+    accessKeyId: config.awsAccessKey,
     secretAccessKey: config.awsSecretKey
-  });
+});
 
-var s3 = new AWS.S3();
- 
 module.exports = {
-    
-    
 
-    uploadVideo : function(req){
 
- 
- 
-        if(req.query.fb_id){
-            
+
+    uploadVideo: function(req) {
+
+
+
+        if (req.query.fb_id) {
+
         }
 
     },
 
 
 
-    uploadAWSVideo : function(req,res){
+    uploadAWSVideo: function(req, res) {
+        var file = req.files.file;
 
- 
+        console.log(req.files, "FOELS\n")
         console.log(req.query);
 
-        if(req.query.fb_id){
-            
+        if (req.query.fb_id) {
+
+            const params = {
+                Bucket: BUCKET_NAME,
+                Key: "myapp" + "/" + "fileName",
+                Body: readStream
+            };
+
+
+
+
+
+
+
+
+
         }
 
     }
- 
- 
+
+
 
 };
-
-
-
- 
--- app/notifications/getNotifications.js
+++ app/notifications/getNotifications.js
@@ -80,7 +80,7 @@ module.exports = {
 
                     code: 201,
 
-                    msg: { response: "Json Parem are missing" }
+                    msg: [{ response: "Json Parem are missing" }]
 
                 })
         }
