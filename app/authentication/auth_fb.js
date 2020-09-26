7bca24dc615d025dbd461b8a662965ce17c6d73e
sarthak <sarthak1singhal@gmail.com>
2020-09-26 14:21:28 +0530

    S

--- app/authentication/auth_fb.js
+++ app/authentication/auth_fb.js
@@ -42,15 +42,225 @@ module.exports = function(app, passport) {
                 // handle success
                 console.log(response, "is response");
 
-                appToken = response.data.access_token;
-                link = 'https://graph.facebook.com/debug_token?input_token=' + userToken + '&access_token=' + appToken
+                let appToken = response.data.access_token;
+                let link = 'https://graph.facebook.com/debug_token?input_token=' + userToken + '&access_token=' + appToken
 
                 axios.get(link)
                     .then(function(response) {
                         // handle success
                         console.log(response.data, " is response 2");
 
-                        user_id = response.data.user_id;
+                        let user_id = response.data.user_id;
+                        if (user_id) {
+                            email = req.body.email.trim().toLowerCase()
+
+                            /*
+                             data: {
+    app_id: '1194749257563443',
+    type: 'USER',
+    application: 'June Social',
+    data_access_expires_at: 1606480674,
+    expires_at: 1606289866,
+    is_valid: true,
+    issued_at: 1601105866,
+    scopes: [ 'email', 'public_profile' ],
+    user_id: '3180757235310807'
+  }
+                            */
+
+
+                            con.query("select * from users where email = ?", [email], function(e, r) {
+                                if (e)
+                                    console.log(e);
+                                let uuid = uniqid();
+                                if (r.length == 0) {
+                                    con.query("insert into users (first_name, last_name,  email, fb_id,profile_pic,signup_type) values (?,?,?,?,?,?,?,?)", [req.body.f_name, req.body.l_name, email, uuid, req.body.profile_pic, req.body.signup_type], function(e, row) {
+
+
+                                        console.log(e);
+                                        var d = {
+                                            "id": uuid,
+                                        }
+                                        const token = jwt.sign(JSON.stringify(d), config.jwt_secret);
+
+
+                                        refresh_token = "";
+
+
+
+
+
+
+
+                                        refresh_token = fx.refresh_token(64);
+                                        let refresh_token_maxage = Math.round(new Date().getTime() / 1000) + jwt_refresh_expiration;
+
+                                        client.SET(d.id, JSON.stringify({
+                                            refresh_token: refresh_token,
+                                            expires: refresh_token_maxage
+                                        }), (err, reply) => {
+
+                                            if (err) {
+                                                console.log(err);
+                                                return;
+                                            }
+
+                                        })
+
+
+
+
+
+
+                                        res.cookie("access_token", token, {
+                                            // secure: true,
+                                            httpOnly: true
+                                        });
+                                        res.cookie("refresh_token", refresh_token, {
+                                            // secure: true,
+                                            httpOnly: true
+                                        });
+
+
+
+
+                                        res.send({
+
+                                            isError: false,
+                                            first_name: req.body.f_name,
+                                            last_name: req.body.l_name,
+                                            isUsername: false,
+                                            app_language: "english",
+                                            email: req.body.email,
+                                            token: token,
+                                            refresh: refresh_token,
+                                            uid: uuid,
+                                            profile_pic: req.body.profile_pic
+
+                                        });
+
+                                    })
+                                } else {
+                                    user = r[0]
+
+
+                                    var d = {
+                                        "id": user.fb_id,
+                                    }
+                                    const token = jwt.sign(JSON.stringify(d), config.jwt_secret);
+
+
+                                    refresh_token = "";
+
+
+
+
+                                    client.GET(user.id, (err, reply) => {
+
+                                        if (err) {
+                                            console.log(err);
+                                            res.sendStatus(500);
+                                            return;
+                                        }
+
+
+
+                                        try {
+                                            reply = JSON.parse(reply);
+
+                                        } catch (_r) {
+
+                                        }
+
+                                        if (reply && reply.refresh_token) {
+
+                                            refresh_token = reply.refresh_token;
+
+
+                                        } else {
+
+
+                                            refresh_token = fx.refresh_token(64);
+                                            let refresh_token_maxage = Math.round(new Date().getTime() / 1000) + jwt_refresh_expiration;
+
+                                            client.SET(user.id, JSON.stringify({
+                                                refresh_token: refresh_token,
+                                                expires: refresh_token_maxage
+                                            }), (err, reply) => {
+
+                                                if (err) {
+                                                    console.log(err);
+                                                    return;
+                                                }
+
+                                            })
+
+
+
+                                        }
+
+
+
+                                        res.cookie("access_token", token, {
+                                            // secure: true,
+                                            httpOnly: true
+                                        });
+                                        res.cookie("refresh_token", refresh_token, {
+                                            // secure: true,
+                                            httpOnly: true
+                                        });
+
+
+                                        let username = "";
+
+                                        let isUsername = false;
+                                        if (r[0].username) {
+
+                                            username = r[0].username
+
+                                            isUsername = true
+                                        }
+
+                                        return res.send({
+
+                                            isError: false,
+                                            first_name: r[0].first_name,
+                                            last_name: r[0].last_name,
+                                            gender: r[0].gender,
+                                            block: r[0].block,
+                                            email: r[0].email,
+                                            profile_pic: r[0].profile_pic,
+                                            app_language: r[0].app_language,
+                                            token: token,
+                                            refresh: refresh_token,
+                                            isUsername: isUsername,
+                                            username: username,
+                                            uid: r[0].fb_id
+
+
+                                        });
+
+
+
+                                    })
+
+                                }
+
+
+
+
+
+
+                            })
+
+                        } else {
+
+                            res.send({
+                                isError: true,
+                                message: "Account not verified"
+                            })
+
+                        }
 
 
 
@@ -68,210 +278,6 @@ module.exports = function(app, passport) {
 
 
 
-        email = req.body.email.trim().toLowerCase()
-
-
-
-
-        con.query("select * from users where email = ?", [email], function(e, r) {
-            if (e)
-                console.log(e);
-            let uuid = uniqid();
-            if (r.length == 0) {
-                con.query("insert into users (first_name, last_name,  email, fb_id) values (?,?,?,?,?,?)", [req.body.f_name, req.body.l_name, req.body.email, uuid], function(e, row) {
-
-
-
-                    var d = {
-                        "id": uuid,
-                    }
-                    const token = jwt.sign(JSON.stringify(d), config.jwt_secret);
-
-
-                    refresh_token = "";
-
-
-
-
-
-
-
-                    refresh_token = fx.refresh_token(64);
-                    let refresh_token_maxage = Math.round(new Date().getTime() / 1000) + jwt_refresh_expiration;
-
-                    client.SET(d.id, JSON.stringify({
-                        refresh_token: refresh_token,
-                        expires: refresh_token_maxage
-                    }), (err, reply) => {
-
-                        if (err) {
-                            console.log(err);
-                            return;
-                        }
-
-                    })
-
-
-
-
-
-
-                    res.cookie("access_token", token, {
-                        // secure: true,
-                        httpOnly: true
-                    });
-                    res.cookie("refresh_token", refresh_token, {
-                        // secure: true,
-                        httpOnly: true
-                    });
-
-
-
-
-                    res.send({
-
-                        isError: false,
-                        first_name: req.body.f_name,
-                        last_name: req.body.l_name,
-                        isUsername: false,
-                        app_language: "english",
-                        email: req.body.email,
-                        token: token,
-                        refresh: refresh_token,
-                        uid: uuid
-
-                    });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                })
-            } else {
-                user = r[0]
-
-
-                var d = {
-                    "id": user.fb_id,
-                }
-                const token = jwt.sign(JSON.stringify(d), config.jwt_secret);
-
-
-                refresh_token = "";
-
-
-
-
-                client.GET(user.id, (err, reply) => {
-
-                    if (err) {
-                        console.log(err);
-                        res.sendStatus(500);
-                        return;
-                    }
-
-
-
-                    try {
-                        reply = JSON.parse(reply);
-
-                    } catch (_r) {
-
-                    }
-
-                    if (reply && reply.refresh_token) {
-
-                        refresh_token = reply.refresh_token;
-
-
-                    } else {
-
-
-                        refresh_token = fx.refresh_token(64);
-                        let refresh_token_maxage = Math.round(new Date().getTime() / 1000) + jwt_refresh_expiration;
-
-                        client.SET(user.id, JSON.stringify({
-                            refresh_token: refresh_token,
-                            expires: refresh_token_maxage
-                        }), (err, reply) => {
-
-                            if (err) {
-                                console.log(err);
-                                return;
-                            }
-
-                        })
-
-
-
-                    }
-
-
-
-                    res.cookie("access_token", token, {
-                        // secure: true,
-                        httpOnly: true
-                    });
-                    res.cookie("refresh_token", refresh_token, {
-                        // secure: true,
-                        httpOnly: true
-                    });
-
-
-                    let username = "";
-
-                    let isUsername = false;
-                    if (r[0].username) {
-
-                        username = r[0].username
-
-                        isUsername = true
-                    }
-
-                    return res.send({
-
-                        isError: false,
-                        first_name: r[0].first_name,
-                        last_name: r[0].last_name,
-                        gender: r[0].gender,
-                        block: r[0].block,
-                        email: r[0].email,
-                        profile_pic: r[0].profile_pic,
-                        app_language: r[0].app_language,
-                        token: token,
-                        refresh: refresh_token,
-                        isUsername: isUsername,
-                        username: username,
-                        uid: r[0].fb_id
-
-
-                    });
-
-
-
-                })
-
-            }
-
-
-
-
-
-
-        })
 
 
 
