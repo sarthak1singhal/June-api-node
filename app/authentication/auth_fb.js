7147b6c46a7df18790d90d29c17c6326cce6a4e8
sarthak <sarthak1singhal@gmail.com>
2020-11-03 00:32:15 +0530

    S

--- app/authentication/auth_fb.js
+++ app/authentication/auth_fb.js
@@ -47,7 +47,7 @@ module.exports = function(app, passport) {
                 let link = 'https://graph.facebook.com/debug_token?input_token=' + userToken + '&access_token=' + appToken
 
                 axios.get(link)
-                    .then(function(response) {
+                    .then(async function(response) {
                         // handle success
                         response.data = response.data.data;
                         console.log(response.data, " is response 2");
