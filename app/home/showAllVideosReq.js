b3793862a66d3f003176021bb263838aa374ede5
sarthak <sarthak1singhal@gmail.com>
2020-11-07 00:34:16 +0530

    S

--- app/home/showAllVideosReq.js
+++ app/home/showAllVideosReq.js
@@ -564,7 +564,7 @@ module.exports = function(app) {
 
                 let [countcomment, fn] = await acon.execute("SELECT count(*) as count from video_comment where video_id=? ", [row_posts[j].id]);
 
-                [liked_count, qq] = await acon.execute("SELECT count(*) as count from video_like_dislike where video_id= ? and fb_id= ?", [row_posts[i].id, fb_id]);
+                [liked_count, qq] = await acon.execute("SELECT count(*) as count from video_like_dislike where video_id= ? and fb_id= ?", [row_posts[j].id, fb_id]);
 
 
 
