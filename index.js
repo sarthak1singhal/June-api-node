241d9792a99daa08f58573648da8530da54cb8e8
sarthak <sarthak1singhal@gmail.com>
2020-11-19 01:09:01 +0530

    S

--- index.js
+++ index.js
@@ -115,6 +115,6 @@ require('./app/profile/getUploadUrl')(app, passport);
     console.log("Srrefsns jf jd fs");
 });
 */
-app.listen(3000, function() {
+app.listen(80, function() {
     console.log("Srrefsns jf jd fs");
 });
