2556552e98290097f99018c199e8c44654154a66
Ansh Nagrath <you@example.com>
2020-11-21 17:49:53 +0530

    port changes

--- index.js
+++ index.js
@@ -115,6 +115,6 @@ require('./app/profile/getUploadUrl')(app, passport);
     console.log("Srrefsns jf jd fs");
 });
 */
-app.listen(8080, function() {
+app.listen(5000, function() {
     console.log("Srrefsns jf jd fs");
 });
