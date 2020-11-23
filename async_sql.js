1be06813f15fa05d4d498c7df438abb5ad789ebc
sarthak <sarthak1singhal@gmail.com>
2020-11-23 17:50:16 +0530

    S

--- async_sql.js
+++ async_sql.js
@@ -1,13 +1,16 @@
 const amysql = require('mysql2/promise');
 readJson = require("r-json");
 const config = readJson(`config.json`);
+var acon = async function() {
+    var acon = await amysql.createConnection({
+        host: config.host,
+        user: config.user,
+        password: config.password,
+        database: config.database
+    });
+    return acon;
+}
 
-var acon = await amysql.createConnection({
-    host: config.host,
-    user: config.user,
-    password: config.password,
-    database: config.database
-});
 
 
 module.exports = acon;
