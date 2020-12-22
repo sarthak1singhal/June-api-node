5b83fbd69ee58ea4eef3bf4c0182fa3fe695540d
Ansh Nagrath <you@example.com>
2020-12-23 00:22:17 +0530

    added functions

---
+++ app/functions/coinstodiamonds.js
@@ -0,0 +1,15 @@
+
+function  coinsToDiamond (coins){
+    return parseInt( coins / 2 )
+    
+}
+
+function diamondToINR( diamond ){
+     return parseInt( diamond / 2 ) 
+
+}
+
+
+
+
+module.exports = { coinsToDiamond , diamondToINR }
